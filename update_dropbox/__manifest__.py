7a6d575f705adc8bc3f2c901d9a1c6248bb5de6b
marvidiaz <marvidiaz@gmail.com>
2018-06-29 11:27:46 -0400

    Update __manifest__.py

--- update_dropbox/__manifest__.py
+++ update_dropbox/__manifest__.py
@@ -23,7 +23,7 @@
 {
     'name': 'Dropbox Backup Manteiner MFH',
     'version': '10.0.0.1.0',
-    'author': "Falcón Solutions",
+    'author': 'Falcon Solutions SpA',
     'maintainer': 'Falcon Solutions',
     'website': 'http://www.falconsolutions.cl',
     'license': 'AGPL-3',
